5c9b04296cacaec0233b9524190018d98a4faed5
Nouhaila Ziyane <nouhaila.ziyane@uit.ac.ma>
2022-11-10 17:13:57 +0100

    commit4

--- lib/screens/auth/resetPass.dart
+++ lib/screens/auth/resetPass.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/keyF.dart';
+import 'package:flutter_application_1/screens/auth/auth.dart';
+
+import 'emailPwd.dart';
 
 class resetPass extends StatefulWidget {
   const resetPass({Key? key}) : super(key: key);
@@ -8,23 +13,38 @@ class resetPass extends StatefulWidget {
 }
 
 class _resetPassState extends State<resetPass> {
+  final TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
-    final TextEditingController emailController = TextEditingController();
     return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         body: Column(children: [
+          Stack(
+        children: <Widget>[
           Container(
             width: w,
             height: h * 0.3,
             decoration: BoxDecoration(
               image: DecorationImage(
                   image: AssetImage("assets/loginimg.png"), fit: BoxFit.cover),
-            ),
-          ),
+            )),
+        Positioned(
+            top: 55,
+            left: 10,
+            child:
+          Container(
+            child:
+              Image.asset("assets/61022.png",
+                height: 60,
+                width: 40,)
+            ,
+          )
+        ),
+          ]),
           SizedBox(
             height: 20,
           ),
@@ -70,7 +90,49 @@ class _resetPassState extends State<resetPass> {
                                 borderRadius: BorderRadius.circular(30))),
                       ),
                     ),
-                  ]))
+                  ])),
+          SizedBox(
+            height: 70,
+          ),
+          GestureDetector(
+              onTap: (){
+                resetPass();
+              },child:
+          Container(
+            width: w*0.5,
+            height: h * 0.08,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              image: DecorationImage(
+                  image: AssetImage("assets/loginbtn.png"), fit: BoxFit.cover),
+              // recognizer: TapGestureRecognizer()..onTap=()=>
+            ),
+            child:
+            Center( child:
+            Text(
+              "Reset Password",
+              style:
+              TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)
+              , )),
+          )),
         ]));
+
+  }
+  Future resetPass() async{
+    showDialog(context: context,
+      barrierDismissible: false,
+      builder: (context)=> Center(child: CircularProgressIndicator()),
+    );
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+      keyF.snackbar("Password Reset Email Sent");
+      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>  EmailPasswordSignup(),));
+
+    }on FirebaseAuthException catch(e){
+      print(e.message);
+      keyF.snackbar(e.message);
+      Navigator.of(context).pop();
+    }
   }
 }
